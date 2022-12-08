a349cc2cd157aa757bd04d2c71792854bfa10a69
Susanna Parolo <77389479+susannaparolo@users.noreply.github.com>
2022-12-08 00:12:11 +0000

    First Commit

---
+++ python-test/tut-four.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+search_term = input("What product do you want to search for? ")
+
+# url_morr = f"https://groceries.morrisons.com/search?entry={product}"
+
+# url = f"https://www.newegg.ca/p/pl?d={search_term}&N=4131"
+# page = requests.get(url).text
+# doc = BeautifulSoup(page, "html.parser")
+
+page_text = doc.find(class_="list-tool-pagination-text").strong
+pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
+
+for page in range(1, pages + 1):
+    url = f"https://www.newegg.ca/p/pl?d={search_term}&N=4131&page={page}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+    
+    div = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    items = div.find_all(text=re.compile(search_term))
+    
+    
+    for item in items:
+        print(item)
+
+
