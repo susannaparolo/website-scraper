f75124cd8e2e1791b39180a1a0aa6c4d104b4a84
selfridges-susanna <susanna.parolo@selfridges.co.uk>
2022-12-13 14:00:53 +0000

    Update :q

--- fetch-scraper/src/index.ts
+++ fetch-scraper/src/index.ts
@@ -14,7 +14,7 @@ export const getShoesData = async (): Promise<ClarksResponse> => {
   const foo = response.text();
   const result: ClarksResponse = JSON.parse(await foo);
   const output = result.products.map(function (value, index) {
-    console.log(`product ${value.name}, rating ${value.averageRating}`);
+    console.log(`product: ${value.name}, rating: ${value.averageRating}`);
   });
   console.log(output.at[0]);
   return result;
